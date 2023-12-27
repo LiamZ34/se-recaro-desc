545309eeb2abfab5551ff23543452b57e37185c6
Test1 1 <williamh2461@gmail.com>
2023-12-27 00:57:31 -0500

    made some changes to the home section

--- src/Home.js
+++ src/Home.js
@@ -4,44 +4,598 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="hero-section">
-        <div class="hero-content">
-        <h1>Your Trusted Cleaning Service</h1>
-        <p>Transforming Spaces, One Cleaning at a Time</p>
-        <button>Contact Us</button>
+   
+
+
+
+
+
+
+    <body>
+    <header>
+      <h1 class="text-center display-2 mb-0">Recaro Podium GF</h1>
+      <h3 class="text-center display-3 text-muted mt-2 mb-5">
+        Ultimate performance without compromise<span style={{ color: '#020202' }}
+          >hello<br /><br
+        /></span>
+        <span
+  style={{
+    boxSizing: 'inherit',
+    outline: 'none',
+    position: 'relative',
+    display: 'inline-block',
+    fontWeight: 'bold',
+    cursor: 'help',
+    color: '#0a0a0a',
+    fontFamily: "'Univers LT W04_45 Light1475948', sans-serif",
+    fontSize: '16px',
+    textAlign: 'center',
+    backgroundColor: '#fefefe',
+    border: '0px none !important',
+  }}
+>
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-replaceable-pads-m-l.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+       
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-hans.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-fia-homologation-5.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-4-5-6-point-belt.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-gfrp.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-gt-sport.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-touring.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+        <span
+          style="
+            box-sizing: inherit;
+            outline: none;
+            position: relative;
+            display: inline-block;
+            font-weight: bold;
+            cursor: help;
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+            border: 0px none !important;
+          "
+        >
+          <picture style="box-sizing: inherit; outline: none">
+            <img
+              loading="lazy"
+              src="https://cdn.recaro-automotive.com/fileadmin/01-products/features/icons/recaro-icon-passive-seat-climate.png?v=1603970888"
+              width="160"
+              height="160"
+              alt=""
+              style="
+                box-sizing: inherit;
+                outline: none;
+                border-style: none;
+                display: inline-block;
+                vertical-align: middle;
+                max-width: 100%;
+                height: 80px;
+                width: auto;
+                margin: 10px 5px 0px;
+              " /></picture>
+              </span>
+      </h3>
+    </header>
+    <section>
+      <p class="text-center display-3 text-muted mt-2 mb-5">
+        <span
+          style="
+            color: #0a0a0a;
+            font-family: 'Univers LT W04_45 Light1475948', sans-serif;
+            font-size: 16px;
+            text-align: center;
+            background-color: #fefefe;
+          "
+          >That's what the RECARO Podium delivers. It is the world’s first performance shell to hold both a general operating permit (the German ABE) 
+          and an 8855-1999 homologation certificate from the FIA (Fédération Internationale de l’Automobile), 
+          meaning it’s approved both for motorsports and for the retrofitting of sporty performance vehicles. 
+          At the Essen Motor Show, RECARO is introducing the new Podium GF with a shell made of glass-fiber-reinforced plastic 
+          (GFRP). Meanwhile, its "big sister" Podium CF – with a shell made of carbon fiber-reinforced polymer (CFRP) 
+          – is celebrating the start of volume production.</span>
+      </p>
+      <div
+        style="
+          text-align: center;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+        "
+      >
+        <a
+          class="btn"
+          style="
+            margin-top: 10px;
+            width: auto;
+            color: rgb(255, 255, 255);
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+          "
+          target="_blank"
+          href="https://cdn.recaro-automotive.com/fileadmin/00-corporate-website/pdf/us/downloads/recaro-brochure-car-us.pdf?v=1603970744"
+          rel="noopener"
+        >
+          DOWNLOAD BROCHURE
+        </a>
+        <a
+          class="btn"
+          style="
+            margin-top: 10px;
+            width: auto;
+            color: rgb(255, 255, 255);
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+          "
+          target="_blank"
+          href="https://cdn.recaro-automotive.com/fileadmin/01-products/documents/hotsheets/recaro-hotsheet-sportster-gt.pdf?v=1688076800"
+          rel="noopener"
+        >
+          DOWNLOAD HOTSHEET
+        </a>
+      </div>
+      <br />
+      <hr
+        style="
+          height: 1px;
+          border: 0;
+          color: #333;
+          background-color: #333;
+          width: 80%;
+          opacity: 10%;
+        "
+      />
+      <p style="text-align: center">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/CTNNpPcQmxY?autoplay=1&rel=0"
+          frameborder="0"
+          allowfullscreen="allowfullscreen"
+        ></iframe>
+      </p>
+      <hr
+        style="
+          height: 1px;
+          border: 0;
+          color: #333;
+          background-color: #333;
+          width: 80%;
+          opacity: 10%;
+        "
+      />
+
+<section style="padding-left: 20px; padding-right: 20px;">
+
+
+      <div style="background-image: url('https://cdn.recaro-automotive.com/typo3conf/ext/wc_recaro_site/Resources/Public/img/ergomed-feature-bg.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size: cover; padding-top: 100px; padding-bottom: 100px;">
+        <div style="justify-content: center; text-align: center; color: white;">
+            <h1  style="text-align: center; margin-bottom: 20px; color: white; font-size: 50px; ">Top Features</h1>
+            <ul style= "list-style-type: circle; display: inline-block; text-align: left;">
+              <li >Competition Seat (FIA 8855-1999)</li>
+              <li >FIA approval for 5 years</li>
+              <li >Suitable for 4-, 5-, 6-point belt (racing use)</li>
+              <li >Dynamic ride performance</li>
+              <li >Seat shell glass fiber reinforced polymer (GFRP)</li>
+            </ul>
+          </div>
+      </div>
+</section>
+
+      <div style="display: flex; flex-direction: row">
+        <div style="width: 100%; text-align: center">
+          <h2 style="text-align: center; margin-bottom: 20px">Key Features</h2>
+          <ul
+            style="
+              list-style-type: circle;
+              display: inline-block;
+              text-align: left;
+            "
+          >
+            <li>Backrest adjustment, manual</li>
+            <li>Integrated headrest</li>
+            <li>Specially formed shoulder support</li>
+            <li>Dynamic ride performance</li>
+            <li>Slim design</li>
+            <li>Suitable for 3-, 4- or 5-point belts</li>
+          </ul>
+        </div>
+        <div style="display: flex; flex-direction: column; width: 100%">
+          <h2 style="text-align: center">Dimensions</h2>
+          <img
+            loading="lazy"
+            style="
+              text-align: center;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+            "
+            src="https://cdn.recaro-automotive.com/fileadmin/01-products/dynamic/us/sportster-gt/dimensions/recaro-sportster-gt-dimensions-us.png?v=1603970890"
+            width="407"
+            height="248"
+            alt=""
+          />
         </div>
       </div>
 
-      <div className="about-section"> 
-      <div className="aboutUs">
+      <div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Sportster GT</th>
 
-        <h2>About Us</h2>
-        <p>
-          Welcome to our cleaning service! With years of experience, we provide top-notch
-          cleaning services tailored to your needs. Our dedicated team is committed to
-          delivering excellence and ensuring your space is spotless.
-        </p>
-      </div>
-      <div className='empty'>
+                <th></th>
+              </tr>
+            </tbody>
+          </table>
 
-      </div>
-      </div>
+          <div>
+            <div>
+              <span>Comfort</span>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Backrest adjustment, manual</td>
 
-      <div className="cta-section">
-        <div className='empty'>
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <span>Lightweight</span>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Composite materials</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <span>Performance</span>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Integrated headrest</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Special lateral support in the backrest area</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Special lateral support in the seat cushion area</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Specially formed shoulder support</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Dynamic ride performance</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Slim design</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <span>Safety</span>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Suitable for 3-, 4- or 5-point belts</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Highly fire-resistant upholstery</td>
+
+                    <td>
+                      <span></span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <span></span>&nbsp;<small>Accessories</small>&nbsp;<span
+            ></span>&nbsp;<small>Options</small>&nbsp;<span></span>&nbsp;<small
+              >Standard features</small
+            >
+          </div>
         </div>
-        <div className='aboutUs'>
-
-        <h2>Our Services</h2>
-        <ul>
-          <li>Professional and Reliable Cleaners</li>
-          <li>Customized Cleaning Plans</li>
-          <li>Quality Service at Affordable Prices</li>
-        </ul>
-        </div>
       </div>
-    </div>
+    </section>
+  </body>
+
+
+
+
+
+
+
   );
 };
 
